f4f995f9f397ff567fb3220b6a7f8fb8a6bc9d0b
Erik Brendel <erik.brendel@student.hpi.de>
2016-04-11 17:00:17 +0200

    License header - kleine Spielerei

--- main/Main.java
+++ main/Main.java
@@ -1,10 +1,17 @@
+/*
+ *  Copyright 2016 
+ *  Markus Brand and Erik Brendel, Potsdam.
+ *  This File is part of a game created
+ *  for LudumDare 35.
+ */
 package main;
 
 /**
  * Main class for LD project
  */
 public class Main {
+
     public static void main(String[] args) {
-        
+
     }
 }
