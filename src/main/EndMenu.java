6173e21920879209c160a0007265d4e96d73947f
Erik Brendel <erik.brendel@student.hpi.de>
2016-04-18 17:23:18 +0200

    text rendering start

--- src/main/EndMenu.java
+++ src/main/EndMenu.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.awt.Font;
+import java.io.InputStream;
 import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
 import static org.lwjgl.opengl.GL11.glDepthMask;
 import static org.lwjgl.opengl.GL11.glDisable;
@@ -7,6 +9,9 @@ import static org.lwjgl.opengl.GL11.glEnable;
 
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.util.vector.Vector3f;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.TrueTypeFont;
+import org.newdawn.slick.util.ResourceLoader;
 import util.Material;
 import util.Mesh;
 import util.MeshInstance;
@@ -16,16 +21,19 @@ import util.Util;
 
 public class EndMenu {
 
-	private MeshInstance background;
-
-	private Shader shader;
-
 	private static Mesh icon;
 	private static Material backgroundMaterial;
+	private static TrueTypeFont font;
+	private static final boolean text_AntiAlias = true;
+
+	private MeshInstance background;
+	private Shader shader;
 
 	static {
 		icon = ObjectLoader.loadObjectEBO("icon.obj");
 		backgroundMaterial = new Material(Util.loadTexture("deathscreen.png"), 0);
+		Font awtFont = new Font("Times New Roman", Font.BOLD, 40);
+		//font = new TrueTypeFont(awtFont, text_AntiAlias);
 	}
 
 	public EndMenu() {
@@ -48,5 +56,7 @@ public class EndMenu {
 		background.render(shader);
 		glDepthMask(true);
 		glEnable(GL_DEPTH_TEST);
+		//Color.white.bind();
+		//font.drawString(100, 50, "THE LIGHTWEIGHT JAVA GAMES LIBRARY", Color.white);
 	}
 }
